9d9fa2dc8ee37d7c989ff6278cffe8d13d89732d
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-04-14 12:16:31 +0900

    user_edit_logout_flash_Implementation
    ユーザー情報のエディット
    ユーザーログアウト機能の実装
    それぞれのアクションに対しての上部フラッシュメッセージの追加

--- db/migrate/20200414014015_devise_create_users.rb
+++ db/migrate/20200414014015_devise_create_users.rb
@@ -7,7 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :password_confirmation, null:  false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20200414014015) do
     t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "password_confirmation",               null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
