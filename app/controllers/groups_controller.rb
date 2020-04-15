533a16527d1654e0302880651fe4505ac2d8e651
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-04-15 15:37:16 +0900

    messege_send_function
    messegeの送信機能の実装
    グループのメンバー表示等の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを修正しました'
+      redirect_to group_messages_path(@group.id), notice: 'グループを修正しました'
     else
       render :edit
     end
